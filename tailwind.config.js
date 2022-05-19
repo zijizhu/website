94138f4d6c9123e35c24a67edfa35b1365d03649
Zi-ji <zhuzhijie1003@outlook.com>
2022-05-19 23:40:46 +1000

    implemented keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,6 +25,27 @@ module.exports = {
       },
       boxShadow: {
         around: '0 0 20px rgb(0 0 0 / 0.1), 0 0 20px rgb(0 0 0 / 0.1)'
+      },
+      animation: {
+        'fade-in-100ms': '1s ease 0s normal forwards 1 fadeIn',
+        'fade-in-150ms': '1.5s ease 0s normal forwards 1 fadeIn',
+        'fade-in-200ms': '2s ease 0s normal forwards 1 fadeIn',
+        'slide-in-200ms': '2.5s ease 0s normal forwards 1 slideIn'
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '50%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        },
+        slideIn: {
+          '0%': { opacity: 0 },
+          '50%': {
+            opacity: 0,
+            transform: 'translateY(25px)'
+          },
+          '100%': { opacity: 1, transform: 'translateY(0%)' }
+        }
       }
     }
   },
