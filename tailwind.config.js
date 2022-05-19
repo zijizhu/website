fa850bed34fd0103076216983bfba7b37e9a60eb
Zi-ji <zhuzhijie1003@outlook.com>
2022-05-19 20:05:47 +1000

    installed tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: [],
+  theme: {
+    fontFamily: {
+      sans: ['Outfit', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        primary: '#4B7FD2',
+        baseText: '#1A2B38',
+        specialText: '#47749A',
+        baseBg: '#F4F5F7',
+        specialBg: '#DFE1E5',
+        selectionBg: '#B9BFCB'
+      }
+    }
+  },
+  plugins: []
+};
