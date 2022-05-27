33e9920a61825d68c736ab8a68e1959da4fd6dc6
Zi-ji <zhuzhijie1003@outlook.com>
2022-05-27 22:38:00 +1000

    project cards wip, added hooks

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,6 +1,9 @@
 import clsx from 'clsx';
 import Image from 'next/image';
 
+import Linking from './icons/Linking';
+import { useWindowSize } from '../hooks';
+import ReactIcon from './icons/ReactIcon';
 import FootstepsImg from '../../public/footsteps.png';
 
 interface ProjectCardProps {
@@ -8,6 +11,9 @@ interface ProjectCardProps {
 }
 
 function ProjectCard({ imgLayout = 'left' }: ProjectCardProps) {
+  const { width } = useWindowSize();
+  const linkingOnLeft = width && width > 768 && imgLayout === 'left';
+
   return (
     <div
       className={clsx([
@@ -17,7 +23,7 @@ function ProjectCard({ imgLayout = 'left' }: ProjectCardProps) {
       ])}
     >
       <div
-        className="hidden md:block shadow-lg rounded-lg w-96 lg:w-144"
+        className="hidden md:block shadow-lg rounded-lg w-112 lg:w-144"
         style={{ fontSize: 0 }}
       >
         <Image src={FootstepsImg} alt="footsteps" className="rounded-lg" />
@@ -29,14 +35,27 @@ function ProjectCard({ imgLayout = 'left' }: ProjectCardProps) {
         ])}
       >
         <h2 className="py-4 text-3xl font-bold">TaskHouse</h2>
-        <h3 className="text-xl font-semibold text-primary">
-          UNSW COMP3900 Capstone project
-        </h3>
+        <a href="https://richardzhu.me/" className="flex items-center">
+          <span className="text-xl font-semibold text-primary">
+            {linkingOnLeft && (
+              <Linking className="inline mx-2 fill-primary w-7 h-7" />
+            )}
+            UNSW COMP3900 Capstone project
+            {!linkingOnLeft && (
+              <Linking className="inline mx-2 fill-primary w-7 h-7" />
+            )}
+          </span>
+        </a>
         <p className="py-4 text-lg lg:text-xl">
           TaskHouse is a collaborative platform for teams to communicate the
           state of their tasks, for users to connect with their collaborators
           through a user-friendly interface.
         </p>
+        <div
+          className={clsx(['flex', { 'md:justify-end': imgLayout === 'left' }])}
+        >
+          <ReactIcon height={40} width={40} />
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
     },
     extend: {
       spacing: {
+        112: '28rem',
         128: '32rem',
         144: '36rem',
         160: '40rem',
